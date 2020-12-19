0b6d7409916b750a6c830fcf878e7d265c5eef73
Mert Erkol <44846524+MrErkol@users.noreply.github.com>
2020-12-19 17:21:33 +0300

    Update User.java

--- User.java
+++ User.java
@@ -1,9 +1,16 @@
-public interface User{
-     String username="";
-     String password="";
-    
+public abstract class User {
+    public String username;
+    public String password;
 
-    public void login();
-    public void sign_up();
+    public boolean login(String username,String password) {
+        return(this.username.equals(username) && this.password.equals(password));
+
+    };
+
+    public void sign_up(String newUsername,String newPassword) {
+        this.username = newUsername;
+        this.password = newPassword;
+
+    }
 
 }

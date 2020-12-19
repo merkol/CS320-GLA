feaecb98d5fa8df774a2198b990f5351e6c9d601
Mert Erkol <44846524+MrErkol@users.noreply.github.com>
2020-12-19 16:07:29 +0300

    Add files via upload

---
+++ m.java
@@ -0,0 +1,9 @@
+public class m {
+
+    public static void main(String[] args){
+        Game g1=new Game("Cyberpunk 1999","s","CDPR",250,100,false);
+        
+        System.out.println(g1.toString());
+    }
+    
+}

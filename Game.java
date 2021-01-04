9d2c72f0c6920068d599e1fd493c60869ba3992c
BO014465 <76252981+BO014465@users.noreply.github.com>
2021-01-04 11:05:26 +0300

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,25 +1,25 @@
+package User;
+
 public class Game{
+	protected int game_id;
     protected  String name;
     protected  String description;
     protected  String publisher;
     protected  float price;
     protected  float score;
     protected  boolean is_multi;
-    
-        
-    
-    
-    public Game(String name,String description,String publisher,float price,float score,boolean is_multi){
-        this.name=name;this.description=description;this.publisher=publisher; this.price=price; this.score=score;this.is_multi=is_multi;} 
-    
-    public String toString(){
-        return this.name+"'s Features:\nDescription: "+this.description+", Publisher: "+this.publisher+", Price: "+this.price+", Score: "+this.score+", Multiplayer: "+this.is_multi;
-    }
-    
-    
+
+    public Game(int game_id,String name,String description,String publisher,float price,float score,boolean is_multi){
+        this.game_id = game_id;
+        this.name=name;
+        this.description=description;
+        this.publisher=publisher;
+        this.price=price;
+        this.score=score;
+        this.is_multi=is_multi;
+        } 
+
 
 }
-
-
 
     

4a3b5a653b00e1f658368fd99cdb969e71d5ea43
Mert Erkol <44846524+MrErkol@users.noreply.github.com>
2021-01-04 11:04:59 +0300

    Delete m.java

--- m.java
+++
@@ -1,9 +0,0 @@
-public class m {
-
-    public static void main(String[] args){
-        Game g1=new Game("Cyberpunk 1999","s","CDPR",250,100,false);
-        
-        System.out.println(g1.toString());
-    }
-    
-}
